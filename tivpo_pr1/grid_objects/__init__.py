5bd154627cbffcc5b69fdd9fc4a6919c72c1347e
Nikita Krasnov <n.krasnov@omp.ru>
2024-09-13 18:05:30 +0300

    Implement GRID_OBJECTS_MAP through hacky reflection

--- tivpo_pr1/grid_objects/__init__.py
+++ tivpo_pr1/grid_objects/__init__.py
@@ -4,8 +4,15 @@ from .objects import GridObject
 
 
 def _generate_grid_objects_map() -> dict[str, GridObject]:
-    grid_objects: dict[str, GridObject] = []
+    grid_objects: dict[str, GridObject] = {}
     for name, cls in inspect.getmembers(objects):
-        if inspect.isclass(cls) and issubclass(cls, GridObject) and cls is not GridObject:
-            grid_objects.ap
+        if (
+            inspect.isclass(cls)
+            and issubclass(cls, GridObject)
+            and cls is not GridObject
+        ):
+            grid_objects[cls().symbol] = cls
     return grid_objects
+
+
+GRID_OBJECTS_MAP: dict[str, GridObject] = _generate_grid_objects_map()
--- tivpo_pr1/parser/parser.py
+++ tivpo_pr1/parser/parser.py
@@ -1,6 +1,6 @@
 from typing import TextIO
 from tivpo_pr1.screen_grid import ScreenGrid
-
+from tivpo_pr1.grid_objects import GRID_OBJECTS_MAP
 
 class Parser:
     """Parse file to construct ScreenGrid."""
@@ -9,6 +9,10 @@ class Parser:
     _current_line_num: int
 
     _screen_grid: ScreenGrid
+
+    def __init__(self) -> None:
+        print(GRID_OBJECTS_MAP)
+
 
     def parse(self, file: TextIO) -> ScreenGrid:
         for line_num, line in enumerate(file):
