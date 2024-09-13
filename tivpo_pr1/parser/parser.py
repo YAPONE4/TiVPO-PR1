e22e4f586efdd716f446ff10e3ad7fc2f6ab38a5
Nikita Krasnov <n.krasnov@omp.ru>
2024-09-13 18:39:26 +0300

    Fix parser errors

--- tivpo_pr1/parser/parser.py
+++ tivpo_pr1/parser/parser.py
@@ -7,21 +7,21 @@ from tivpo_pr1.grid_objects import GRID_OBJECTS_MAP
 class Parser:
     """Parse file to construct ScreenGrid."""
 
-    _current_line: str
+    _current_line_contents: str
     _current_line_num: int
 
-    _parsed_objects: list[GridObject]
+    _parsed_objects: list[GridObject] = []
 
     def parse(self, file: TextIO) -> list[GridObject]:
         for line_num, line in enumerate(file):
             self._current_line_num = line_num + 1
-            self._current_line = line
+            self._current_line_contents = line
             self._parse_line()
 
         return self._parsed_objects
 
     def _parse_line(self):
-        for index, symbol in enumerate(self._current_line.split()):
+        for index, symbol in enumerate(self._current_line_contents.split()):
             if symbol in GRID_OBJECTS_MAP:
                 object: GridObject = GRID_OBJECTS_MAP[symbol]()
                 object.line_num = self._current_line_num
@@ -29,5 +29,5 @@ class Parser:
                 self._parsed_objects.append(object)
             else:
                 raise InvalidSymbolError(
-                    f'Unknown symbol "{symbol}" on line {self._current_line} at index {index}'
+                    f'Unknown symbol "{symbol}" on line {self._current_line_num} at index {index}'
                 )
