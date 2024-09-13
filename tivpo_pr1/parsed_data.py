c976b3b5151638088ea577ef337ba2f3582cdfa0
Nikita Krasnov <n.krasnov@omp.ru>
2024-09-13 15:22:27 +0300

    Add ParsedData class

---
+++ tivpo_pr1/parsed_data.py
@@ -0,0 +1,5 @@
+class ParsedData:
+    """Data that was read from the file and should be drawn."""
+
+    def __init__(self):
+        print("ParsedData createad")
--- tivpo_pr1/parser.py
+++ tivpo_pr1/parser.py
@@ -1,3 +1,11 @@
+from typing import TextIO
+
+
 class Parser:
+    """Parse file to construct ParsedData."""
+
     def __init__(self):
         print("Parser createad")
+
+    def parse(file: TextIO):
+        pass
