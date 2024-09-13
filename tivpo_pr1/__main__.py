3e4639a9e311767b104340a858789416c595d370
Nikita Krasnov <n.krasnov@omp.ru>
2024-09-13 18:57:44 +0300

    Improve examples

--- tivpo_pr1/__main__.py
+++ tivpo_pr1/__main__.py
@@ -1,8 +1,12 @@
 from pathlib import Path
+from tivpo_pr1.grid_objects.objects import GridObject
 from tivpo_pr1.parser import Parser
 
 path_to_input_file: Path = Path(Path.cwd(), "examples/input_example.txt")
 
+symbols: list[GridObject]
 with open(path_to_input_file) as f:
-    a = Parser().parse(f)
+    symbols = Parser().parse(f)
 
+for symbol in symbols:
+    print(f"{symbol.symbol} at line {symbol.line_num} at index {symbol.index}")
