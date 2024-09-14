92cf85d3bc49e8e961384659048513c778f3495c
Nikita Krasnov <n.krasnov@omp.ru>
2024-09-14 13:04:13 +0300

    Fix formatting: reformat __main__.py file

--- tivpo_pr1/__main__.py
+++ tivpo_pr1/__main__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from tivpo_pr1.grid_objects.objects import GridObject
 from tivpo_pr1.parser import Parser
 from turtle import *
-from tivpo_pr1.printer import * 
+from tivpo_pr1.printer import *
 
 path_to_input_file: Path = Path(Path.cwd(), "examples/input_example.txt")
 
@@ -14,16 +14,16 @@ for symbol in symbols:
     print(f"{symbol.symbol} at line {symbol.line_num} at index {symbol.index}")
 
 
-printer:Printer = Printer()
-'''printer.drawArrowLeft(1, 1)
-printer.drawArrowRight(0, 1)
-printer.drawArrowLeftRight(0, 2)
-printer.drawArrowLeftRight(1, 2)
-printer.drawCircle(2, 2)
-printer.drawSquare(0, 3)
-printer.drawArrowLeft(1, 3)
-printer.drawCircle(2, 1)
-printer.drawArrowLeftRight(3, 1)
-printer.drawArrowLeft(3, 2)'''
+printer: Printer = Printer()
+# printer.drawArrowLeft(1, 1)
+# printer.drawArrowRight(0, 1)
+# printer.drawArrowLeftRight(0, 2)
+# printer.drawArrowLeftRight(1, 2)
+# printer.drawCircle(2, 2)
+# printer.drawSquare(0, 3)
+# printer.drawArrowLeft(1, 3)
+# printer.drawCircle(2, 1)
+# printer.drawArrowLeftRight(3, 1)
+# printer.drawArrowLeft(3, 2)
 printer.draw_whole_picture(symbols)
 printer.show_screen()
